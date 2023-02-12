d49140c00ba68a2f1c6dab3308c703f7e48a4629
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-02-12 19:28:36 +0100

    Update go_go.sh

--- payload/go_go.sh
+++ payload/go_go.sh
@@ -14,13 +14,13 @@ cd /root/SDA_ALL/main_oct0pus/
 
 
 chmod +x *
-ngrok authtoken $NGROK_TOKENS
-ngrok http 9001 > /dev/null &
-export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")" && echo $WEBHOOK_URL
+#ngrok authtoken $NGROK_TOKENS
+#ngrok http 9001 > /dev/null &
+#export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")" && echo $WEBHOOK_URL
 while true
 do
 	echo "NEW ..............."
 	dbus-uuidgen > /var/lib/dbus/machine-id
-	cd /root/SDA_ALL/main_oct0pus/
+	cd /root/SDA_ALL/main_oct0pus_van/
 	python3 google_let.py
 done
